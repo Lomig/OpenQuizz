f78ec1496b86e07885d12471f34784f1fc5606c2
Lomig Enfroy <dunwael@gmail.com>
2020-05-26 13:48:09 +0200

    Initial Commit

---
+++ OpenQuizz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenQuizz
+//
+//  Created by Lomig Enfroy on 26/05/2020.
+//  Copyright © 2020 Lomig Enfroy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
